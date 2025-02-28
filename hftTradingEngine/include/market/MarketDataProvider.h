ecc8c82e1119c190050178a8f3045ebd49c83ab0
moomie1 <rileyk2809@gmail.com>
2025-02-27 23:32:16 -0800

    made some additions again

---
+++ hftTradingEngine/include/market/MarketDataProvider.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <chrono>
+#include <memory>
+
+class MarketProvider;
+
+struct PriceLevel {
+	long double price;
+	long long volume;
+};
+
+struct Trade {
+
+};
